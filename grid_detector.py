76452e2b59222facfddb058b468a35879ac16ac0
Dumitras Daniel <dumitras.daniell@gmail.com>
2023-10-27 00:30:50 +0200

    Created table with every cell of the table. Need to find OCR model compatible with android to recognize digits.

--- grid_detector.py
+++ grid_detector.py
@@ -1,32 +1,54 @@
 import numpy as np
 import cv2 as cv
 import sys
+import pytesseract
+
+def preProcessImage(image_grid):
+    #changing color to gray for better image processing
+    gray = cv.cvtColor(image_grid, cv.COLOR_BGR2GRAY)
+
+    blur = cv.GaussianBlur(gray,(5,5),0)
+
+    #function to create a binary image
+    threshold = cv.threshold(blur,0,255,cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
+
+    #function to return the connected components from the image
+    #the second argument is the connectivity (4 or 8) to determine nbr of neighbors
+    #the third is the threshold type
+    return cv.connectedComponentsWithStats(threshold,4,cv.CV_32S)
+
+def extractNumbers(image_grid):
+    gray = cv.cvtColor(image_grid, cv.COLOR_BGR2GRAY)
+    height, width = gray.shape
+    box_height = height // 9
+    box_width = width // 9
+    boxes = []
+    for row in range(9):
+        for col in range(9):
+            # Calculate the coordinates for each box
+            y1 = row * box_height
+            y2 = (row + 1) * box_height
+            x1 = col * box_width
+            x2 = (col + 1) * box_width
+            box = gray[y1:y2, x1:x2]
+            boxes.append(box)
+    nbrs = []
+    #for b in boxes:
+        #number = pytesseract.image_to_string(b,config='--psm 10 --oem 3 -c tessedit_char_whitelist=123456789')
+        #nbrs.append(number)
+    return nbrs
 
 #importing file
-file_normal = "sudoku_grid_test.png"
-file_rotated = "sudoku_rotated_grid.png"
-img = cv.imread(file_rotated)
+file = ["sudoku_grid_test.png","sudoku_rotated_grid.png"]
+img = cv.imread(file[0])
 
-#changing color to gray for better image processing
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur = cv.GaussianBlur(gray,(5,5),0)
-
-#function to create a binary image
-threshold = cv.threshold(blur,0,255,cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
-
-#function to return the connected components from the image
-#the second argument is the connectivity (4 or 8) to determine nbr of neighbors
-#the third is the threshold type
-out = cv.connectedComponentsWithStats(threshold,4,cv.CV_32S)
+out = preProcessImage(img)
 
 #numLabels = number of components
 #labels = position of pixel
 #stats = stat of component (coordinates and area)
 #centroid = center of component
 (numLabels, labels, stats, centroid) = out
-x = cv.CC_STAT_LEFT
-y = cv.CC_STAT_TOP
-
 component = (labels==1).astype("uint8")*255
 
 #get largest area of the image
@@ -45,8 +67,11 @@ sudoku_corners[3] = points[np.argmax(np.diff(points, axis=1))]
 output_size = (500,500)
 perspective_matrix = cv.getPerspectiveTransform(sudoku_corners, np.array([[0, 0], [output_size[0], 0], [output_size[0], output_size[1]], [0, output_size[1]]], dtype=np.float32))
 warped_sudoku = cv.warpPerspective(img, perspective_matrix, output_size)
-cv.imshow("Sudoku grid", warped_sudoku)
 
-k = cv.waitKey(0) 
+numbers = extractNumbers(warped_sudoku)
+
+cv.imshow("Sudoku grid", warped_sudoku)
+k = cv.waitKey(0)
+
 if k == ord("q"):
     sys.exit()
